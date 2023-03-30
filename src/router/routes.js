ecd722c7b76a7152486ef9570bb6e77eb53e67c6
Nigina Gafurova <nigina.gafurova.2001@mail.ru>
2023-03-30 17:50:04 +0300

    Добавлена новая страница MainPage c боковой панелью

--- src/router/routes.js
+++ src/router/routes.js
@@ -7,6 +7,7 @@ const routes = [
       { path: '', name:'loginDefault', component: () => import('pages/Login.vue') },
       { path: 'login', name:'login', component: () => import('pages/Login.vue') },
       { path: 'register', name:'register', component: () => import('pages/Register.vue') },
+      { path: 'main', name:'main', component: () => import('pages/MainPage.vue') },
     ]
   },
 
