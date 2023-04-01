2e22e5d77a369010a97656595279a5b12e9200c3
SofiyaKH <119045257+SofiyaKH@users.noreply.github.com>
2023-04-01 16:31:42 +0300

    Реализована авторизация пользователя

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,42 +1,88 @@
 <template>
-    <q-page padding>
-        <q-form class="row justify-center" @submit.prevent="">
-            <p class="col-12 text-h5 text-center">Login</p>
-            <div class="col-md-4 col-sm-6 col-xs-10 q-gutter-y-lg">
-                <q-input
-                    label="Email"
-                    v-model="form.email"
-                />
+  <q-page padding>
+    <q-form class="row justify-center" @submit.prevent="">
+      <p class="col-12 text-h5 text-center">Login</p>
+      <div class="col-md-4 col-sm-6 col-xs-10 q-gutter-y-lg">
+        <q-input label="Email" v-model="form.email" />
 
-                <q-input
-                    label="Password"
-                    v-model="form.password"
-                /> 
+        <q-input label="Password" v-model="form.password" />
 
-                <div class="q-mt-lg">
-                    <q-btn outline size="md" color="black" label="Login" class="full-width" type="submit"/>     
-                </div>
+        <div class="q-mt-lg">
+          <q-btn
+            outline
+            size="md"
+            color="black"
+            label="Login"
+            class="full-width"
+            type="submit"
+            @click="signIn"
+          />
+        </div>
 
-                <div >
-                    <q-btn flat size="md" color="black" label="Register" class="full-width" to="/register"/>     
-                </div>     
-            </div>
-        </q-form>
-    </q-page>
+        <div>
+          <q-btn
+            flat
+            size="md"
+            color="black"
+            label="Register"
+            class="full-width"
+            to="/register"
+          />
+        </div>
+      </div>
+    </q-form>
+  </q-page>
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref } from "vue";
+import { useMutation } from "@vue/apollo-composable";
+import gql from "graphql-tag";
+
 export default defineComponent({
-  name: 'PageLogin',
-  setup(){
+  name: "PageLogin",
+  setup() {
     const form = ref({
-        email: "",
-        password: ""
-    })
-    return{
-        form
-    }
-  }
-})
+      email: "",
+      password: "",
+    });
+
+    const { mutate: signInUser } = useMutation(gql`
+      mutation UserSignIn($input: UserSignInInput!) {
+        userSignIn(input: $input) {
+          recordId
+          record {
+            token_type
+            expires_in
+            access_token
+            refresh_token
+          }
+          status
+        }
+      }
+    `);
+
+    const signIn = async () => {
+      const { data } = await signInUser({
+        input: {
+          login: form.value.email,
+          password: form.value.password,
+        },
+      });
+
+      console.log(data.userSignIn);
+      console.log(data.userSignIn.status);
+
+      if (data.userSignIn.status === 200) {
+        window.location.href = "#/main";
+      }
+    };
+
+    return {
+      form,
+      signIn,
+      signInUser,
+    };
+  },
+});
 </script>
