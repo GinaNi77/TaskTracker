cc46f05e08da7b0469244d49fd253fc56a4f6a7e
Nigina Gafurova <nigina.gafurova.2001@mail.ru>
2023-04-03 23:37:17 +0300

    Исправления в дизайне компонента addPerformerUser

--- src/components/AddPerformerUser.vue
+++ src/components/AddPerformerUser.vue
@@ -1,26 +1,74 @@
 <template>
-    <div>Исполнители</div>
-
-    <q-form class="row justify-center" @submit.prevent="signIn">
-            <p class="col-12 text-h5 text-center">Login</p>
+    <q-page padding>
+        <q-form class="row justify-center" @submit.prevent="addPerformer">
+            <p class="col-12 text-h5 text-center">Добавить исполнителя</p>
             <div class="col-md-4 col-sm-6 col-xs-10 q-gutter-y-lg">
                 <q-input
-                    label="Email"
-                    v-model="form.email"
+                    label="Имя"
+                    v-model="form.name"
                 />
 
                 <q-input
-                    label="Password"
-                    v-model="form.password"
+                    label="Фамилия"
+                    v-model="form.surname"
+                />
+
+                <q-input
+                    label="Почта"
+                    v-model="form.email"
                 /> 
 
                 <div class="q-mt-lg">
-                    <q-btn outline size="md" color="black" label="Login" class="full-width" type="submit"/>     
-                </div>
-
-                <div >
-                    <q-btn flat size="md" color="black" label="Register" class="full-width" to="/register"/>     
-                </div>     
+                    <q-btn outline size="md" color="black" label="Добавить" class="full-width" type="submit"/>     
+                </div>    
             </div>
         </q-form>
+    </q-page>
 </template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import { useMutation } from '@vue/apollo-composable'
+import gql from "graphql-tag";
+
+export default defineComponent({
+  setup(){
+    const form = ref({
+        name:"",
+        email: "",
+        surname: ""
+    })
+
+     const { mutate: userGroupInviteUser } = useMutation(gql`
+            mutation inviteUserToGroup($input: InviteUserToGroupInput!) {
+                inviteUserToGroup(input: $input) {
+                status
+            }
+        }`
+    )
+
+    const addPerformer = async () => {
+       const {data} = await userGroupInviteUser(
+        {
+            "input": {
+            "name": form.value.name,
+            "surname": form.value.surname,
+            "email": form.value.email,
+            "page_group_id": "4753316581813399177"
+            }
+        }) 
+        resetForm()
+    };
+
+    const resetForm = () => {
+      form.value.email = "",
+      form.value.surname = "",
+      form.value.name = ""
+    };
+
+    return{
+        form, addPerformer
+    }
+  }
+})
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -8,6 +8,7 @@ const routes = [
       { path: 'login', name:'login', component: () => import('pages/Login.vue') },
       { path: 'register', name:'register', component: () => import('pages/Register.vue') },
       { path: 'main', name:'main', component: () => import('pages/MainPage.vue') },
+      { path: 'addUser', name:'addUser', component: () => import('components/addPerformerUser.vue') }
     ]
   },
 
