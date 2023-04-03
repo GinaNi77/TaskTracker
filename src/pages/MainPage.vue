1072cbccec768a1ab8fa12c5ae79b9b471216dff
SofiyaKH <119045257+SofiyaKH@users.noreply.github.com>
2023-04-03 22:54:31 +0300

    Реализовано дерево страниц

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,52 +1,23 @@
 <template>
   <q-drawer
-    v-model="drawer"
-    show-if-above
-    :mini="miniState"
-    @mouseover="miniState = false"
-    @mouseout="miniState = true"
-    :width="200"
-    :breakpoint="500"
+    v-model="leftDrawerOpen"
+    :width="400"
+    :breakpoint="300"
+    class="bg-grey-1"
     bordered
     :class="$q.dark.isActive ? 'bg-grey-9' : 'bg-grey-3'"
   >
-    <q-scroll-area class="fit" :horizontal-thumb-style="{ opacity: 0 }">
-      <q-list padding>
-        <q-item>
-          <div class="team-div">
-            <div class="flex">
-              <q-item-section avatar>
-                <q-icon name="inbox" />
-              </q-item-section>
-
-              <q-item-section> Команда </q-item-section>
-            </div>
-            <q-list class="team-list">
-              <q-item clickable v-for="item in childrenPages">
-                <q-item-section>
-                  {{ item.title }}
-                </q-item-section></q-item
-              >
-            </q-list>
-          </div>
-        </q-item>
-
-        <q-item active clickable v-ripple>
-          <q-item-section avatar>
-            <q-icon name="star" />
-          </q-item-section>
-
-          <q-item-section> Модули </q-item-section>
-        </q-item>
-
-        <q-item clickable v-ripple>
-          <q-item-section avatar>
-            <q-icon name="send" />
-          </q-item-section>
-
-          <q-item-section> Мои задачи </q-item-section>
-        </q-item>
-      </q-list>
+    <q-scroll-area
+      class="fit"
+      :width="400"
+      :horizontal-thumb-style="{ opacity: 0 }"
+    >
+      <q-tree
+        v-if="result"
+        :nodes="treePages"
+        node-key="label"
+        v-model:selected="selected"
+      />
     </q-scroll-area>
   </q-drawer>
 </template>
@@ -59,44 +30,68 @@ import gql from "graphql-tag";
 
 export default defineComponent({
   setup() {
-    const childrenPages = ref([]);
+    const treePages = ref([]);
+    const parentPages = ref([]);
+    const selected = ref();
+    const leftDrawerOpen = ref(true);
 
-    const { result, onResult } = useQuery(gql`
-      query {
-        page(id: "6932152816939161311") {
-          id
-          parent_id
-          page_type
-          title
-          content
-          icon
-          level
-          is_public
-          position
-          config
-          is_block
-          children(perPage: 10, page: 1) {
+    const { result, loading, error, onResult, refetch } = useQuery(
+      gql`
+        query parentPages {
+          rootPages {
             data {
               id
+              parent_id
+              page_type
               title
+              content
+              icon
+              level
+              is_public
+              position
+              config
+              children {
+                data {
+                  id
+                  title
+                }
+              }
+              created_at
+              updated_at
             }
           }
-          created_at
-          updated_at
         }
-      }
-    `);
+      `
+    );
 
     onResult(() => {
-      childrenPages.value = result.value.page.children.data;
-      console.log(childrenPages.value);
+      parentPages.value = result.value.rootPages.data;
+      console.log(parentPages.value);
+
+      parentPages.value.forEach((page) => {
+        let treeElem = {
+          label: page.title,
+          // avatar: page.icon,
+          children: page.children.data.map((elem) => {
+            elem = { label: elem.title, id: elem.id };
+            return elem;
+          }),
+        };
+        treePages.value.push(treeElem);
+      });
+
+      selected.value = treePages.value[0].label;
+      console.log(treePages.value[0].label);
     });
+
     return {
       drawer: ref(false),
       miniState: ref(true),
-      onResult,
       result,
-      childrenPages,
+      parentPages,
+      treePages,
+      selected,
+      leftDrawerOpen,
     };
   },
 });
