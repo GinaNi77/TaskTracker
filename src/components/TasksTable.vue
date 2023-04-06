e74c3012be0be00595ad7b477770b727a12ce466
Nigina Gafurova <nigina.gafurova.2001@mail.ru>
2023-04-07 00:58:30 +0300

    добавлена функция изменения задачи

--- src/components/TasksTable.vue
+++ src/components/TasksTable.vue
@@ -29,7 +29,7 @@
           <td>{{task.property5.fullname.first_name}} {{task.property5.fullname.last_name}}</td>
           <td>{{task.property9.name}}</td>
           <td>
-            <q-btn class="bg-teal-10 text-white" icon="edit"></q-btn>
+            <q-btn class="bg-teal-10 text-white" icon="edit"  @click="getTaskId(task.id)"></q-btn>
           </td>
           <td>
             <q-btn class="bg-red-10 text-white" @click="deleteTasks(task.id)" icon="delete"></q-btn>
@@ -40,6 +40,63 @@
     <div class="flex justify-center q-mb-lg">
       <q-btn color="black" outline to="/addTask">Добавить задачу</q-btn>
     </div>
+
+    <q-dialog v-model="alert">
+      <q-card>
+        <q-form class="row justify-center" @submit.prevent="updateTasks">
+            <p class="col-12 text-h5 text-center">Изменить Задачу</p>
+            <div class="col-md-4 col-sm-6 col-xs-10 q-gutter-y-lg">
+                <q-input
+                    label="Название"
+                    v-model="title"
+                />
+
+                <q-input
+                    label="Описание"
+                    v-model="description"
+                />
+
+                <q-input label="Исполнитель" v-model="performerUser">
+                    <template #append>
+                        <q-icon name="arrow_drop_down" class="cursor-pointer"></q-icon>
+                        <q-popup-proxy>
+                                <q-list>
+                                    <q-item clickable v-close-popup  v-for="user in performerUsers" :key="user.index" @click="getUserId(user.id)">
+                                    <q-item-section>
+                                        <q-item-label>{{user.fullname.first_name}}</q-item-label>
+                                    </q-item-section>
+                                    <q-item-section>
+                                        <q-item-label>{{user.fullname.last_name}}</q-item-label>
+                                    </q-item-section>
+                                    </q-item>
+                                </q-list>
+                        </q-popup-proxy>
+                    </template>
+                </q-input>
+
+
+                <q-input label="Модуль" v-model=" moduleId">
+                    <template #append>
+                        <q-icon name="arrow_drop_down" class="cursor-pointer"></q-icon>
+                        <q-popup-proxy>
+                                <q-list>
+                                    <q-item clickable v-close-popup  v-for="item in modulesList" :key="item.index" @click="getModuleId(item.id)">
+                                    <q-item-section>
+                                        <q-item-label>{{item.name}}</q-item-label>
+                                    </q-item-section>
+                                    </q-item>
+                                </q-list>
+                        </q-popup-proxy>
+                    </template>
+                </q-input>
+
+                <div class="q-mt-lg">
+                    <q-btn outline size="md" color="black" label="Изменить" class="full-width q-mb-md" type="submit"/>     
+                </div>
+            </div>
+        </q-form>
+      </q-card>
+    </q-dialog>
     
 </template>
 
@@ -53,7 +110,20 @@ export default defineComponent({
     setup() {
 
         const tasksList = ref([])
+        const modulesList = ref([])
+        const performerUsers = ref([])
+        const alert= ref(false)
+        const title = ref('')
+        const description = ref('')
+        const performerUser = ref('')
+        const moduleId = ref()
+        const taskId = ref()
 
+        const getTaskId=(id)=>{
+          alert.value = true
+          taskId.value = id
+          
+        }
         const { result, onResult } = useQuery(
       gql`
         query getModules {
@@ -104,6 +174,7 @@ export default defineComponent({
       }
     );
 
+
     // name - названия задания
     // property 3 - описание задания
     // property 8 - айдишник статуса задачи
@@ -116,6 +187,71 @@ export default defineComponent({
         console.log( tasksList.value)
     });
     
+
+    const getResponsible = ()=>{
+        const { result, onResult } = useQuery(
+      gql`
+        query {
+          get_group(id: "4753316581813399177") {
+            name
+            subject {
+              id
+              type_id
+              email {
+                email
+              }
+              fullname {
+                first_name
+                last_name
+              }
+            }
+          }
+        }
+      `
+    );
+
+    onResult(() => {
+      performerUsers.value = result.value.get_group.subject;
+      console.log(performerUsers.value);
+    });
+
+      return { 
+        onResult
+        }
+    }
+
+    getResponsible()
+
+      const getModules = () =>{
+    const { result, onResult } = useQuery(
+      gql`
+        query getModules {
+        paginate_type1(page: 1, perPage: 100) {
+        data {
+            id
+            type_id
+            author_id
+            level
+            position
+            created_at
+            updated_at
+            name
+                    }
+                }
+            }`
+    );
+
+    onResult(() => {
+      modulesList.value = result.value.paginate_type1.data
+      console.log(modulesList.value)
+    });
+
+    return { 
+        onResult
+        }
+    }
+
+    getModules()
 
     const {mutate: deleteTask} = useMutation(gql`
       mutation($id: String!) {
@@ -134,8 +270,79 @@ export default defineComponent({
 
     }
 
+    const getUserId = (id) => {
+            performerUser.value = id
+    }
+
+    const getModuleId = (id) => {
+            moduleId.value = id
+    }
+
+    const {mutate: updateTask}=useMutation(gql`
+    mutation ($id: String!,$input: update_type2_input!) {
+    update_type2(id: $id, input: $input) {
+      status
+      recordId
+      record {
+        id
+        type_id
+        author_id
+        level
+        position
+        created_at
+        updated_at
+        name
+        property5 {
+          id
+          user_id
+          fullname {
+            first_name
+            last_name
+          }
+        }
+        property3
+        property8
+        property9{
+          name
+          property4{
+            fullname{
+              first_name
+              last_name
+            }
+          }
+          
+          
+        }
+        
+    }
+  }
+}
+    `)
+
+    const updateTasks = async() => {
+      const {data} = await updateTask(
+        {
+  "id": taskId.value,
+  "input":{
+	"name": title.value,
+  "property3": description.value,
+  "property8": "8536411824694842134",
+  "property5":{
+  	"2730894142110796608":performerUser.value
+              },
+  "property9":{
+    "6591698446779899108":moduleId.value
+              }
+  }
+    
+  }
+        )
+}
+
     return{
-           onResult,  tasksList, deleteTasks
+           onResult,  tasksList, deleteTasks, alert, getModules, 
+           modulesList, performerUsers, getResponsible, title,
+           description, getUserId, getModuleId, performerUser, moduleId, taskId, getTaskId, updateTasks
         }
         
     },
